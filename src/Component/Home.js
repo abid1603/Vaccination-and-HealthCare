f455147ce98c8bdab106d9916748f8e50d58fdae
abid1603 <88293639+abid1603@users.noreply.github.com>
2021-10-20 23:46:43 +0600

    home page

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import '../Component/Home.css';
 
 class Home extends Component {
     render() {
         return (
             <div>
-                Home
+                <div className="content">
+                        <div className="background-image">
+                                <h1>
+                                    VACCINATION <br /> <span>& HEALTH CARE</span>
+                                </h1>
+                                <p> We work for vaccination and health care for <br /> pregnent women and new born baby to ensure <br /> their Risk free future.</p>
+                                <button type="button" class="btn btn-primary">Learn More</button>
+                        </div>
+                </div>
             </div>
         );
     }
